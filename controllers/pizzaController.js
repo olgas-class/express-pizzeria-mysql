25f90a5591f0ad0c5d3689bc19ff8ea19c6567c8
olga_demina <olga.demina1410@gmail.com>
2025-01-23 10:14:56 +0100

    feat: env variables and ingredients query in shoq pizza

--- controllers/pizzaController.js
+++ controllers/pizzaController.js
@@ -1,13 +1,21 @@
 const connection = require("../data/db");
 
 const index = (req, res) => {
-  const sql = "SELECT * FROM `pizzas`";
+  const environment = process.env.ENVIRONMENT; 
+
+  const sql = "SELECT * FROM `pizzass`";
 
   connection.query(sql, (err, pizze) => {
     if (err) {
-      return res.status(500).json({
+      const responseObj = {
         message: "Errore interno del server",
-      });
+      }
+
+      if(environment === "development") {
+        responseObj.details = err.stack;
+      }
+
+      return res.status(500).json(responseObj);
     } else {
       return res.status(200).json({
         status: "success",
@@ -21,21 +29,44 @@ const show = (req, res) => {
   const id = req.params.id;
 
   const sql = "SELECT * FROM `pizzas` WHERE id = ?";
+  const ingredientsSql = `
+    SELECT ingredients.* 
+    FROM ingredients
+    JOIN ingredient_pizza
+    ON ingredients.id = ingredient_pizza.ingredient_id
+    JOIN pizzas
+    ON pizzas.id = ingredient_pizza.pizza_id
+    WHERE pizzas.id = ?
+  `;
 
   connection.query(sql, [id], (err, pizze) => {
     if (err) {
       return res.status(500).json({
-        message: "errore interno del server",
+        message: "errore interno del server"
       });
     } else if (pizze.length === 0) {
       return res.status(404).json({
         message: "Pizza non trovata",
       });
     } else {
-      // Nel caso del bonus si fa la seconda query con join qui
-      return res.status(200).json({
-        status: "success",
-        data: pizze[0],
+      // prendiamo gli ingredienti collegati alla pizza
+      connection.query(ingredientsSql, [id], (err, ingredienti) => {
+        if (err) {
+          return res.status(500).json({
+            message: "errore interno del server",
+          });
+        } else {
+          const datiPizza = pizze[0];
+          console.log(ingredienti, pizze[0]);
+
+          return res.status(200).json({
+            status: "success",
+            data: {
+              ...datiPizza,
+              ingredienti,
+            },
+          });
+        }
       });
     }
   });
